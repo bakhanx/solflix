5b585a755d4599c2280345624339551e20016091
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2022-06-20 21:36:27 +0900

    nav top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,7 @@
+const Home = ()=>{
+    return <div style={{backgroundColor:"whitesmoke", height:"200vh"}}>
+        <h1>Home</h1>
+    </div>;
+}
+
+export default Home;
