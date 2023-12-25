bce3bb328794ad7278d010d8c906d07b7f71a764
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2023-12-25 21:35:02 +0900

    basename 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,43 +7,46 @@ import Tv from "./Routes/Tv";
 import ErrorComponents from "./Components/ErrorComponents";
 import Detail from "./Components/Detail";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-        errorElement: <ErrorComponents />,
-        children: [
-          {
-            path: "/movies/:movieId",
-            element: <></>,
-            errorElement: <ErrorComponents />,
-          },
-        ],
-      },
-      {
-        path: "search",
-        element: <Search />,
-        errorElement: <ErrorComponents />,
-      },
-      {
-        path: "/tvs",
-        element: <Tv />,
-        children: [
-          {
-            path: "/tvs/:tvId",
-            element: <></>,
-            errorElement: <ErrorComponents />,
-          },
-        ],
-        errorElement: <ErrorComponents />,
-      },
-    ],
-    errorElement: <NotFound />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+          errorElement: <ErrorComponents />,
+          children: [
+            {
+              path: "/movies/:movieId",
+              element: <></>,
+              errorElement: <ErrorComponents />,
+            },
+          ],
+        },
+        {
+          path: "search",
+          element: <Search />,
+          errorElement: <ErrorComponents />,
+        },
+        {
+          path: "/tvs",
+          element: <Tv />,
+          children: [
+            {
+              path: "/tvs/:tvId",
+              element: <></>,
+              errorElement: <ErrorComponents />,
+            },
+          ],
+          errorElement: <ErrorComponents />,
+        },
+      ],
+      errorElement: <NotFound />,
+    },
+  ],
+  { basename: `${process.env.PUBLIC_URL}` }
+);
 
 export default router;
